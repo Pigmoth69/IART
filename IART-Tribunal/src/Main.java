657a6ef0a0af77c02f692354063319f1d76d901c
Daniel Silva Reis <pigmoth@gmail.com>
2016-04-07 17:16:23 +0100

    cenas

--- IART-Tribunal/src/Main.java
+++ IART-Tribunal/src/Main.java
@@ -1,5 +1,7 @@
 import Parser.ParseCity;
 
+import java.io.IOException;
+
 /**
  * Created by danny on 07/04/2016.
  */
@@ -7,8 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("IART-Court");
-        /*"https://pt.wikipedia.org/wiki/Lista_de_municípios_de_Portugal_por_população";*/
-        ParseCity parser = new ParseCity();
+        String URL = new String("https://pt.wikipedia.org/wiki/Lista_de_municípios_de_Portugal_por_população");
+        try {
+            ParseCity parser = new ParseCity(URL);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("IART-FINAL");
     }
--- IART-Tribunal/src/Parser/ParseCity.java
+++ IART-Tribunal/src/Parser/ParseCity.java
@@ -2,7 +2,12 @@ package Parser;
 
 import jdk.nashorn.internal.parser.JSONParser;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 /**
  * Created by danny on 07/04/2016.
@@ -11,18 +16,53 @@ public class ParseCity {
 
     private ArrayList<City> cityList;
     private String citiesURL = new String();
+    private String htmlCode= new String();
 
-    ParseCity(String citiesURL){
+    public ParseCity(String citiesURL) throws IOException {
         if(citiesURL.isEmpty())
             System.err.println("Please, choose a url");
         else {
             this.citiesURL = citiesURL;
-            loadCities();
+            loadHtml();
+            FileOutputStream f = new FileOutputStream("merda.txt");
+            f.write(htmlCode.getBytes());
+            f.close();
         }
     }
 
+    void loadHtml() throws IOException{
+        URL url = new URL(citiesURL);
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        String inputLine;
+        StringBuilder builder = new StringBuilder();
+        while ((inputLine = in.readLine()) != null){
+            builder.append(inputLine);
+        }
+        htmlCode = builder.toString();
+        in.close();
+    }
+
     void loadCities(){
-        JSONParser jsonParser = new JSONParser();
+        System.out.println("Reading html document of cities.. Please wait.");
+        URL url = null;
+        try {
+            url = new URL(citiesURL);
+            FileInputStream is = (FileInputStream)url.getContent();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            FileOutputStream file = new FileOutputStream("merda.txt");
+        } catch (MalformedURLException e) {
+            System.err.println("URL constructor error");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Get URL error..");
+            e.printStackTrace();
+        }
+
+
+
+
+
+
 
         /*JSONObject obj = new JSONObject();
         JSONObject object = jsonParser.getJSONFromUrl(url);

13bbf25e545bbca5c633049aeaf0c320508ff64a
Daniel Silva Reis <pigmoth@gmail.com>
2016-04-14 16:04:32 +0100

    novo proj

--- IART-Tribunal/src/Parser/ParseCity.java
+++ IART-Tribunal/src/Parser/ParseCity.java
@@ -1,13 +1,19 @@
 package Parser;
 
-import jdk.nashorn.internal.parser.JSONParser;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
+
+import javax.lang.model.util.Elements;
+import javax.swing.text.Document;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
 
 /**
  * Created by danny on 07/04/2016.
@@ -20,16 +26,14 @@ public class ParseCity {
 
     public ParseCity(String citiesURL) throws IOException {
         if(citiesURL.isEmpty())
-            System.err.println("Please, choose a url");
+            System.err.println("Please, enter a url");
         else {
             this.citiesURL = citiesURL;
             loadHtml();
-            FileOutputStream f = new FileOutputStream("merda.txt");
-            f.write(htmlCode.getBytes());
-            f.close();
+            //parseHtml();
         }
     }
-
+    /*@Brief this functions loads a html page from the web and store it on a file.*/
     void loadHtml() throws IOException{
         URL url = new URL(citiesURL);
         BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -39,9 +43,26 @@ public class ParseCity {
             builder.append(inputLine);
         }
         htmlCode = builder.toString();
+        FileOutputStream f = new FileOutputStream("merda.txt");
+        f.write(htmlCode.getBytes());
+        f.close();
         in.close();
     }
+    void parseHtml() throws ParserConfigurationException, IOException, SAXException {
+       // Document doc = Jsoup.parse(htmlCode);
+       // Elements citiesHtml = doc.getElementsByAttributeValue("style", "text-align: center;");
 
+        /*
+        Elements citiesHtml = doc.getElementsByAttributeValue("style", "text-align: center;");
+
+        for (Element aCitiesHtml : citiesHtml) {
+            String name = aCitiesHtml.children().get(1).text();
+            String population = aCitiesHtml.children().get(2).text();
+            cities.add(new City(name, parsePopulation(population)));
+        }*/
+
+        System.out.println("Finished extracting city names and populations");
+    }
     void loadCities(){
         System.out.println("Reading html document of cities.. Please wait.");
         URL url = null;
@@ -60,13 +81,24 @@ public class ParseCity {
 
 
 
+/*
+         Document doc = Jsoup.parse(html);
+        Elements citiesHtml = doc.getElementsByAttributeValue("style", "text-align: center;");
+
+        for (Element aCitiesHtml : citiesHtml) {
+            String name = aCitiesHtml.children().get(1).text();
+            String population = aCitiesHtml.children().get(2).text();
+            cities.add(new City(name, parsePopulation(population)));
+        }
+
+        System.out.println("Finished extracting city names and populations");*/
 
 
-
-
-        /*JSONObject obj = new JSONObject();
+/*
+        JSONObject obj = new JSONObject();
         JSONObject object = jsonParser.getJSONFromUrl(url);
         String content=object.getString("json key");*/
+
     }
 
 
